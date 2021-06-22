31ce5d7c438ff3fecf00bc49a333f391c8ba8354
Silas Huereca <43799078+silashuereca@users.noreply.github.com>
2021-06-22 12:20:34 -0500

    chore: continue working on refactoring v-model for input component

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,22 @@ module.exports = {
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
     'plugin:vue/vue3-recommended',
+    'prettier'
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
-	rules: {
-		'vue/singleline-html-element-content-newline': 'off',
-		'vue/multiline-html-element-content-newline': 'off',
+  rules: {
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 5,
+        "allowFirstLine": true
+      },
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }]
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
   }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="p-20">
     This is where a brief description of a welcome message for any contributer
     or maybe some guidelines on how to contribute or get started
   </div>
