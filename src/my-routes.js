266273a417a6f39cc9e9258d1b129085d257744e
Silas Huereca <43799078+silashuereca@users.noreply.github.com>
2021-07-25 09:12:52 -0500

    refactor: project structure and setup

--- src/my-routes.js
+++ src/my-routes.js
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './pages/Home.vue';
-import InputPage from './pages/Input.vue';
-import PricingPage from './pages/PricingPage.vue';
+import FormInput from './pages/FormInput.vue';
 
 export const router = createRouter({
 	history: createWebHistory(),
 	routes: [
 		{ path: '/', name: 'home-page', component: Home },
-		{ path: '/input', name: 'input-page', component: InputPage },
-		{ path: '/pricing-page', name: 'pricing-page', component: PricingPage}
+		{ path: '/form-input', name: 'input-page', component: FormInput },
 	]
-})
+});
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,3 @@
 <template>
-  <div>
-    This is where a brief description of a welcome message for any contributer
-    or maybe some guidelines on how to contribute or get started
-  </div>
+  <div>Welcome to Vue Me Outside :)</div>
 </template>
