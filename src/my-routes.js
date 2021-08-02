7e6c8ea9a68ebb6d2732a6dcdabcf86acf878dac
Silas Huereca <43799078+silashuereca@users.noreply.github.com>
2021-08-01 19:15:56 -0500

    chore: code clean up and finish out the documentation

--- src/my-routes.js
+++ src/my-routes.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './pages/Home.vue';
 import FormInput from './pages/FormInput.vue';
+import FormCheckbox from './pages/FormCheckbox.vue';
 
 export const router = createRouter({
 	history: createWebHistory(),
 	routes: [
 		{ path: '/', name: 'home-page', component: Home },
 		{ path: '/form-input', name: 'input-page', component: FormInput },
+		{ path: '/form-checkbox', name: 'checkbox-page', component: FormCheckbox}
 	]
 });
---
+++ src/pages/FormCheckbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input-checkbox id="checkbox" v-model="test" type="checkbox"></input-checkbox>
+  </div>
+</template>
+
+<script>
+import InputCheckbox from "../component-library/form-checkbox/checkbox.vue";
+export default {
+  components: {
+    InputCheckbox,
+  },
+  data: function () {
+    return {
+      test: true,
+    };
+  },
+  watch: {
+    test(value) {
+      console.log("Checkbox", value);
+    },
+  },
+};
+</script>
